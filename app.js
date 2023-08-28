e9b08dd08000a9c81fe2c08eb0b5e818dd3bade9
Maria Shalbanova <mariashalbanova@yandex.ru>
2023-08-28 17:05:54 +0300

    fix: change PORT

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const routes = require('./routes');
 const errorHandler = require('./middlewares/error-handler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+const { PORT = 3001, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 const app = express();
 
